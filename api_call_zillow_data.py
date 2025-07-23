c953ee5577dbaf479b09f5725da7f0442209a72f
catatonicmark <104708453+catatonicmark@users.noreply.github.com>
2025-07-23 14:22:45 -0600

    update api_call_zillow_data
    update to fix api call from nasdaq, working on fixing connecting to sql server

--- api_call_zillow_data.py
+++ api_call_zillow_data.py
@@ -3,31 +3,35 @@ import nasdaqdatalink
 import zipfile
 import pymssql
 import hashlib
+import csv
+
+NASDAQ_DATA_LINK_API_KEY =   nasdaqdatalink.read_key('/Users/tylerpruitt/Desktop/ZILLOW/data_link_apikey.rtf')
 
 def define_api_key():
-    nasdaqdatalink.ApiConfig.api_key = nasdaqdatalink.read_key()
+    nasdaqdatalink.ApiConfig.api_key = nasdaqdatalink.read_key('/Users/tylerpruitt/Desktop/ZILLOW/data_link_apikey.rtf')
 
 def connect_to_sql_server():
-    connection = pd.read_csv('account_info.csv')
-    con=pymssql.connect(connection['host'],connection['username'],connection['password'],connection['db'])
+    connection = pd.read_csv('/Users/tylerpruitt/Desktop/ZILLOW/account_info.csv')
+    connection = connection.to_dict('list')
+    con=pymssql.connect(connection['host'][0], connection['username'][0], connection['password'][0], connection['db'][0])
     cursor=con.cursor()
     return cursor
 
 
 def extract_from_NASDAQ(big_data_table: str): 
-    filename = big_data_table.replace('/', '_')
-    nasdaqdatalink.export_table(big_data_table, f"{filename}.zip")
+    edited_filename = big_data_table.replace('/', '_')
+    nasdaqdatalink.export_table(big_data_table, filename=f"{edited_filename}.zip", qopts={'export': 'true'})
     try:
-        with zipfile.ZipFile(filename + ".zip", 'r') as zip_ref:
+        with zipfile.ZipFile(f'/Users/tylerpruitt/Desktop/ZILLOW/{edited_filename}', 'r') as zip_ref:
             zip_ref.extractall
-            print(f"Successfully extracted all files from {filename}.zip to working directory.")
+            print(f"Successfully extracted all files from {edited_filename}.zip to working directory.")
     except FileNotFoundError:
-        print(f"Error: The file {filename}.zip was not found.")
+        print(f"Error: The file {edited_filename}.zip was not found.")
     except zipfile.BadZipFile:
-        print(f"Error: {filename}.zip is not a valid ZIP file or is corrupted.")
+        print(f"Error: {edited_filename}.zip is not a valid ZIP file or is corrupted.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-    data = pd.read_csv(f'{filename}.csv', dtype={'indicator_id':'category', 'region_id':'category'}, parse_dates=['date'])
+    data = pd.read_csv(f'{edited_filename}.csv', dtype={'indicator_id':'category', 'region_id':'category'}, parse_dates=['date'])
     return data
 
 def read_data_from_SQL(table_name: str, cursor):
@@ -41,8 +45,24 @@ def get_from_NASDAQ(table_code: str):
 
 def compare_data(NASDAQ_data: pd.DataFrame, sql_data: pd.DataFrame):
     def row_hash(df):
-        return df.astype(str).agg('||'.join, axis=1.apply(lambda x: hashlib.md5(x.encode()).hexdigest()))
+        return df.astype(str).agg('||'.join, axis=1).apply(lambda x: hashlib.md5(x.encode()).hexdigest())
     NASDAQ_hashes = row_hash(NASDAQ_data)
     sql_hashes = row_hash(sql_data)
     new_rows = NASDAQ_data[~NASDAQ_hashes.isin(sql_hashes)]
     return new_rows
+
+def clean_file(df: pd.DataFrame):
+    df = df.replace({",":""}, regex=True)
+    return df
+
+print("API Key defined")
+#zillow_data_NQ = extract_from_NASDAQ('ZILLOW/DATA')
+#zillow_data_NQ = pd.read_csv('/Users/tylerpruitt/Desktop/ZILLOW/ZILLOW_DATA.csv', dtype={'indicator_id':'category','region_id':'category'},parse_dates=['date'])
+print("Data extracted from NASDAQ")
+cursor = connect_to_sql_server()
+print("Connected to SQL Server")
+zillow_data_SQL = read_data_from_SQL('[zillow-data-all].[dbo].[ZILLOW_DATA]', cursor)
+print("Data received from SQL Server")
+new_rows = compare_data(zillow_data_NQ, zillow_data_SQL)
+print("Differences calculated")
+print(new_rows)
