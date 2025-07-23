5106d5ac9d6523b313c3a9ab7211a4f348fa6678
catatonicmark <104708453+catatonicmark@users.noreply.github.com>
2025-07-23 09:35:47 -0600

    Update api_call_zillow_data.py
    creating functions to update the db with new csv data

--- api_call_zillow_data.py
+++ api_call_zillow_data.py
@@ -1,24 +1,48 @@
 import pandas as pd
-import numpy as np
 import nasdaqdatalink
 import zipfile
- 
-print(nasdaqdatalink.ApiConfig.api_key)
-""""
-zip_file_path     = nasdaqdatalink.export_table('ZILLOW/DATA', filename='ZILLOW_DATA.zip')
-try:
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall
-        print(f"Successfully extracted all files from '{zip_file_path}' to working directory.")
-except FileNotFoundError:
-    print(f"Error: The file '{zip_file_path}' was not found.")
-except zipfile.BadZipFile:
-    print(f"Error: '{zip_file_path}' is not a valid ZIP file or is corrupted.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+import pymssql
+import hashlib
+
+def define_api_key():
+    nasdaqdatalink.ApiConfig.api_key = nasdaqdatalink.read_key()
+
+def connect_to_sql_server():
+    connection = pd.read_csv('account_info.csv')
+    con=pymssql.connect(connection['host'],connection['username'],connection['password'],connection['db'])
+    cursor=con.cursor()
+    return cursor
 
 
-zillow_data       = pd.read_csv('ZILLOW_DATA.csv', dtype={'indicator_id':'category', 'region_id':'category'}, parse_dates=['date'])
-zillow_indicators = nasdaqdatalink.get_table('ZILLOW/INDICATORS',paginate=True)
-zillow_regions    = nasdaqdatalink.get_table('ZILLOW/REGIONS',paginate=True)
-"""
+def extract_from_NASDAQ(big_data_table: str): 
+    filename = big_data_table.replace('/', '_')
+    nasdaqdatalink.export_table(big_data_table, f"{filename}.zip")
+    try:
+        with zipfile.ZipFile(filename + ".zip", 'r') as zip_ref:
+            zip_ref.extractall
+            print(f"Successfully extracted all files from {filename}.zip to working directory.")
+    except FileNotFoundError:
+        print(f"Error: The file {filename}.zip was not found.")
+    except zipfile.BadZipFile:
+        print(f"Error: {filename}.zip is not a valid ZIP file or is corrupted.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    data = pd.read_csv(f'{filename}.csv', dtype={'indicator_id':'category', 'region_id':'category'}, parse_dates=['date'])
+    return data
+
+def read_data_from_SQL(table_name: str, cursor):
+    sql_query = f"SELECT * FROM {table_name}"
+    sql_data = pd.read_sql(sql_query, cursor)
+    return sql_data
+
+def get_from_NASDAQ(table_code: str):
+    data = nasdaqdatalink.get_table(table_code,paginate=True)
+    return data
+
+def compare_data(NASDAQ_data: pd.DataFrame, sql_data: pd.DataFrame):
+    def row_hash(df):
+        return df.astype(str).agg('||'.join, axis=1.apply(lambda x: hashlib.md5(x.encode()).hexdigest()))
+    NASDAQ_hashes = row_hash(NASDAQ_data)
+    sql_hashes = row_hash(sql_data)
+    new_rows = NASDAQ_data[~NASDAQ_hashes.isin(sql_hashes)]
+    return new_rows
